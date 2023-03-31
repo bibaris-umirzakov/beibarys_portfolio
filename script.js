e19b543e4699d9f50c95593328b81c6a49000d23
Beibarys Omirzakiv <bibarisumirzakov0231201@gmail.com>
2023-04-01 04:51:39 +0600

    Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Beginner FullStake"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
